0ca34de693b2cc9fa8831f6b2fa712beadcfbfa1
yldzmamak <yildiz.unal@param.com.tr>
2023-07-27 14:40:06 +0300

    ..

--- environments/app/development.ts
+++ environments/app/development.ts
@@ -4,6 +4,6 @@ import { IAppConfig } from "./index";
 
 export const developmentConfig: IAppConfig = {
   apiUrl: "https://rickandmortyapi.com/api",
-  loginApiUrl: "http://localhost:3000",
+  loginApiUrl: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000",
   version: packageInfo.version
 };
--- src/services/Http/userLogin.service.ts
+++ src/services/Http/userLogin.service.ts
@@ -1,7 +1,10 @@
 import { ILogin } from "@/types/interfaces/authentication/authentication";
 
+import appConfig from "../../../environments/app";
+
+
 export const loginUserService = async (request: ILogin) => {
-  const LOGIN_API_ENDPOINT = `http://localhost:3000/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
+  const LOGIN_API_ENDPOINT = `${appConfig.loginApiUrl}/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
 
   const parameters = {
     method: "GET",
