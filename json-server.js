8d1446d47d037b54644ee9b42c8baeb26f64b058
yldzmamak <yildiz.unal@param.com.tr>
2023-07-27 13:46:15 +0300

    fixx

--- json-server.js
+++ json-server.js
@@ -13,3 +13,53 @@ app.use((req, res, next) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+// netlify/functions/json-server.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('path/to/db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+exports.handler = server.createHandler({
+  path: '/.netlify/functions/json-server',
+});
+
+// netlify/functions/json-server.js
+const fetch = require('node-fetch');
+
+exports.handler = async (event) => {
+  const request = JSON.parse(event.body);
+  const LOGIN_API_ENDPOINT = `https://fastidious-bombolone-a9ae5a.netlify.app/.netlify/functions/json-server/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
+
+  try {
+    const response = await fetch(LOGIN_API_ENDPOINT, {
+      headers: {
+        'Access-Control-Allow-Origin': 'https://fastidious-bombolone-a9ae5a.netlify.app',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ message: 'Login failed' }),
+      };
+    }
+
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Server error' }),
+    };
+  }
+};
