bb329e5bcff4a28e8172c4581ad81c08f2147aa4
yldzmamak <yildiz.unal@param.com.tr>
2023-07-27 13:12:34 +0300

    fixed

--- src/services/Http/userLogin.service.ts
+++ src/services/Http/userLogin.service.ts
@@ -1,17 +1,24 @@
 import { ILogin } from "@/types/interfaces/authentication/authentication";
 
 export const loginUserService = async (request: ILogin) => {
-  const LOGIN_API_ENDPOINT = `http://localhost:3000/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
-  
+  const isLocal = window.location.hostname === "localhost";
+  let LOGIN_API_ENDPOINT;
+
+  if (isLocal) {
+    LOGIN_API_ENDPOINT = `http://localhost:3000/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
+  } else {
+    LOGIN_API_ENDPOINT = `https://your-netlify-site.netlify.app/.netlify/functions/json-server/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
+  }
+
   const parameters = {
     method: "GET",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
   };
 
   const response = await fetch(LOGIN_API_ENDPOINT, parameters);
-  
+
   const json = await response.json();
   return json[0];
 };
