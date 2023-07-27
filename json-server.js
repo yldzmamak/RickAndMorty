ccc48e0127610a420526bc964c759c0a5af279a3
yldzmamak <yildiz.unal@param.com.tr>
2023-07-27 13:07:31 +0300

    fix

--- json-server.js
+++ json-server.js
@@ -13,3 +13,16 @@ app.use((req, res, next) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+// netlify/functions/json-server.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('path/to/db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+exports.handler = server.createHandler({
+  path: '/.netlify/functions/json-server',
+});
--- src/services/Http/userLogin.service.ts
+++ src/services/Http/userLogin.service.ts
@@ -1,9 +1,7 @@
 import { ILogin } from "@/types/interfaces/authentication/authentication";
 
 export const loginUserService = async (request: ILogin) => {
-  console.log(process.env.NODE_ENV);
-  
-  const LOGIN_API_ENDPOINT = `http://json-server/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
+  const LOGIN_API_ENDPOINT = `http://localhost:3000/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
   
   const parameters = {
     method: "GET",
