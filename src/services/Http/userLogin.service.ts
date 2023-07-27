74b3755cf17bb68a73c68adac8b32ac3dcdfca5b
yldzmamak <yildiz.unal@param.com.tr>
2023-07-27 13:45:39 +0300

    fixx

--- src/services/Http/userLogin.service.ts
+++ src/services/Http/userLogin.service.ts
@@ -7,13 +7,13 @@ export const loginUserService = async (request: ILogin) => {
   if (isLocal) {
     LOGIN_API_ENDPOINT = `http://localhost:3000/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
   } else {
-    LOGIN_API_ENDPOINT = `https://64c246a73d697400086cf332--fastidious-bombolone-a9ae5a.netlify.app/.netlify/functions/json-server/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
+    LOGIN_API_ENDPOINT = `https://fastidious-bombolone-a9ae5a.netlify.app/.netlify/functions/json-server/users?username=${request.CLIENT_USERNAME}&password=${request.CLIENT_PASSWORD}`;
   }
 
   const parameters = {
     method: "GET",
     headers: {
-      "Access-Control-Allow-Origin": "https://64c246a73d697400086cf332--fastidious-bombolone-a9ae5a.netlify.app",
+      "Access-Control-Allow-Origin": "https://fastidious-bombolone-a9ae5a.netlify.app",
       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
       "Access-Control-Allow-Headers": "Content-Type",
       "Content-Type": "application/json",
